e85a10c43321dd474fb326079fd42a24efff5a94
hinto <hinto@rambler.ru>
2023-03-25 11:12:55 +0700

    Импортирует crypto

--- project/apps/users/src/app/task-user/task-user-memory.repository.ts
+++ project/apps/users/src/app/task-user/task-user-memory.repository.ts
@@ -2,6 +2,7 @@ import { CRUDRepository } from '@project/util/util-types';
 import { TaskUserEntity } from './task-user.entity';
 import { User } from '@project/shared/app-types';
 import { Injectable } from '@nestjs/common';
+import crypto from 'crypto';
 
 @Injectable()
 export class TaskUserMemoryRepository implements CRUDRepository<TaskUserEntity, string, User> {
