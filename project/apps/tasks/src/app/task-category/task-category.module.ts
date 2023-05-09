e5c88df52e41e0e0ad9d0eacf46d5a473e45ca88
hinto <hinto@rambler.ru>
2023-05-09 18:09:15 +0700

    Реализует проверку роли пользователя при создании категории

--- project/apps/tasks/src/app/task-category/task-category.module.ts
+++ project/apps/tasks/src/app/task-category/task-category.module.ts
@@ -2,9 +2,10 @@ import { TaskCategoryController } from './task-category.controller';
 import { TaskCategoryService } from './task-category.service';
 import { TaskCategoryRepository } from './task-category.repository';
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from '../authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule],
   controllers: [TaskCategoryController],
   providers: [TaskCategoryService, TaskCategoryRepository],
   exports: [TaskCategoryRepository]
